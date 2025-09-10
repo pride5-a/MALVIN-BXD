f02e5ff16e8462dfa162364564efc59ad4e2e54a
pride5-a <pridekusvbz@gmail.com>
2025-09-10 17:24:34 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~gz0hSQrJ#DavJ99Sj1sFr61EG74sJQjX9XyznQhhZhrtCUuhXBvA",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
